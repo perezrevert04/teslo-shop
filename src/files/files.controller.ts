14bd8745cd73c1862ef8c11b2d860abc3cf02c00
Carles Pérez Revert <48178327+perezrevert04@users.noreply.github.com>
2023-04-13 12:49:08 +0200

    Getting image from server

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,10 +1,14 @@
 import {
   BadRequestException,
   Controller,
+  Get,
+  Param,
   Post,
+  Res,
   UploadedFile,
   UseInterceptors
 } from '@nestjs/common';
+import { Response } from 'express';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
 import { FilesService } from './files.service';
@@ -13,6 +17,16 @@ import { fileFilter, fileNamer } from './helpers/index.helper';
 @Controller('files')
 export class FilesController {
   constructor(private readonly filesService: FilesService) {}
+
+  @Get('product/:imageName')
+  findProductImage(
+    @Res() res: Response,
+    @Param('imageName') imageName: string
+  ) {
+    const path = this.filesService.getStaticProductImage(imageName);
+
+    return res.status(403).sendFile(path);
+  }
 
   @Post('product')
   @UseInterceptors(
@@ -29,6 +43,8 @@ export class FilesController {
       throw new BadRequestException('No file uploaded');
     }
 
-    return file;
+    const secureUrl = `${file.filename}`;
+
+    return secureUrl;
   }
 }
