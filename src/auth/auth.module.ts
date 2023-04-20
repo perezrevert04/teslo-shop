eb9c25a1a15d65736f504cbb506de1e11caf2ade
Carles Pérez Revert <48178327+perezrevert04@users.noreply.github.com>
2023-04-20 17:35:02 +0200

    JWT async module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,14 +5,17 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { User } from './entities/user.entity';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([User]),
     PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.registerAsync({
-      useFactory: async () => ({
-        secret: process.env.JWT_SECRET,
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET'),
         signOptions: { expiresIn: '1d' }
       })
     })
