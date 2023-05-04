218470816bb8f6891b82f137791aebdc72ecdbaa
Carles Pérez Revert <48178327+perezrevert04@users.noreply.github.com>
2023-05-04 17:54:44 +0200

    Import auth in another modules

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from 'src/auth/auth.module';
 import { ProductImage } from './entities/product-image.entity';
 import { Product } from './entities/product.entity';
 import { ProductsController } from './products.controller';
 import { ProductsService } from './products.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Product, ProductImage])],
+  imports: [AuthModule, TypeOrmModule.forFeature([Product, ProductImage])],
   exports: [ProductsService],
   controllers: [ProductsController],
   providers: [ProductsService]
