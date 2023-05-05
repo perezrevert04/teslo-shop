872f6d129f7bf8c9a6c6d6d12ef9cd8309769ed5
Carles Pérez Revert <48178327+perezrevert04@users.noreply.github.com>
2023-05-05 16:46:40 +0200

    Check auth status

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -31,6 +31,12 @@ export class AuthController {
     return this.authService.login(loginUserDto);
   }
 
+  @Get()
+  @Auth()
+  checkAuthStatus(@GetUser() user: User) {
+    return this.authService.checkAuthStatus(user);
+  }
+
   @Get('private')
   @UseGuards(AuthGuard())
   testingPrivateRoute(
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -56,6 +56,13 @@ export class AuthService {
     };
   }
 
+  async checkAuthStatus(user: User) {
+    return {
+      ...user,
+      token: this.getJwtToken({ id: user.id })
+    };
+  }
+
   private handleDBError(error: any): never {
     if (error.code === '23505') throw new BadRequestException(error.detail);
     console.log(error);
